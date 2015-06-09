63bba25db29775ffd9864c85f8c32a0322cccddc
Rei Medeiros <rm091291@gmail.com>
2015-06-09 10:49:01 -0300

    Atualizaçao do projeto de estudos JavaWeb com JSF e Hibernate !

---
+++ JavaWeb/src/java/model/Cores.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ *
+ * @author rei-medeiros
+ */
+
+
+public class Cores {
+
+    public String cores[];
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
